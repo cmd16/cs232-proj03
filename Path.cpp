771ee1d27ecc16301ddca41bd1d3e006c6d6c287
Daniel Ackuaku <dma2@gold27.cs.calvin.edu>
2019-04-01 23:27:04 -0400

    Removed wrong Path constructor class method

--- Path.cpp
+++ Path.cpp
@@ -15,25 +15,8 @@
  * return: Null
  */
 Path::Path() {
-	// char * pch;								// initializing a character pointer
-	// new version
-//	char * tempPath = getenv("PATH");
-//	strcpy(pPath, tempPath);
-// 	// old version
-// 	pPath = getenv("PATH");					// populating the character pointer pPath with the PATH
-// 	pch = strtok(pPath, ":NULL");				// splits the chars into chunks any time the ":" appears
-
-
-	
-
-// /* Loop that stores each directory in PATH in a vector */
-// 	while (pch != NULL) {
-// 		directory.push_back(pch);
-// 		pch = strtok(NULL, ":");			// after ":" a NULL is inserted into the character array causing the loop to return to the top
-// 	}
 
 	string newParse = getenv("PATH");
-	// vector<string> directory;
 	stringstream stream (newParse);
 	string transfer;
 	while(getline(stream, transfer, ':')){
